1925fb341486323593965c581d52f457a65bc594
Manuel de la Peña <mdelapenya@gmail.com>
2023-08-04 13:35:13 +0200

    chore: start dev dependencies using tc-go and init functions

---
+++ dev_dependencies.go
@@ -0,0 +1,51 @@
+//go:build dev
+// +build dev
+
+package main
+
+import (
+	"context"
+	"log"
+	"path/filepath"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/modules/postgres"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+func init() {
+	ctx := context.Background()
+
+	c, err := postgres.RunContainer(ctx,
+		testcontainers.WithImage("postgres:15.3-alpine"),
+		postgres.WithInitScripts(filepath.Join(".", "testdata", "dev-db.sql")),
+		postgres.WithDatabase("users-db"),
+		postgres.WithUsername("postgres"),
+		postgres.WithPassword("postgres"),
+		testcontainers.WithWaitStrategy(
+			wait.ForLog("database system is ready to accept connections").
+				WithOccurrence(2).WithStartupTimeout(5*time.Second)),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	connStr, err := c.ConnectionString(ctx, "sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	// check the connection to the database
+	conn, err := pgx.Connect(ctx, connStr)
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close(ctx)
+
+	// append the containers as a dev dependency
+	App.DevDependencies = append(App.DevDependencies, c)
+	App.UsersConnection = connStr
+	log.Println("Users database started successfully")
+}
---
+++ testdata/dev-db.sql
@@ -0,0 +1,1 @@
+CREATE TABLE IF NOT EXISTS users (id serial, uuid varchar(255), birthdate varchar(255), latitude float, longitude float, created_at timestamp, PRIMARY KEY (id));
